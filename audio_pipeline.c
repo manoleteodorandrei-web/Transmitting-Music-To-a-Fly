0ab89f85963a91421a3845a1ddb78ccdcec8fa81
TeoMan <teoman@Ultron.home.com>
2026-09-12 21:51:32 +0300

    intial project scaffold

---
+++ audio_pipeline.c
@@ -0,0 +1,1 @@
+// Handles the microphone stream and uses FFT to slice the continuous sound wave into discrete frequency bins.
---
+++ connecttome.c
@@ -0,0 +1,2 @@
+// Manages the heavy memory allocation 
+// dynamic structures and using p0ointer arithmetics to build the masssive graph of simulate neurons and synapses  
